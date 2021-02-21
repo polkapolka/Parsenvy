c63a40a40014d81807bfdaf111145553581838a7
polkapolka <pgreenwo@gmail.com>
2021-02-21 11:29:35 -0800

    adding the docstring for str. fixed a typo in the tuple string.

--- docs/source/str.rst
+++ docs/source/str.rst
@@ -1,3 +1,15 @@
 ######
 String
 ######
+
+.. autofunction:: parsenvy.parsenvy.str
+
+
+Usage
+-----
+
+.. code-block:: python
+
+    >>> import parsenvy
+    >>> parsenvy.str('STR_ENV_VAR')
+    "Hello, World"
--- parsenvy/parsenvy.py
+++ parsenvy/parsenvy.py
@@ -155,6 +155,17 @@ def str(
     env_var: builtins.str,
     default: Optional[builtins.str] = None,
 ) -> Optional[builtins.str]:
+    """
+    Parse environment variable value into a string.
+
+    Args:
+        env_var (str): Name of desired environment variable.
+        default (string, optional): Optional fallback value.
+
+    Returns:
+        string (optional): Environment variable typecast into a string.
+
+    """
     if env_var == "":
         return default
 
